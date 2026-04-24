0a6b1ee2f9f4132fdb2d9ae8a3fe5c49e47e25ca
Diode-exe <DiodesCAER@protonmail.com>
2026-04-23 22:37:25 -0500

    2.1.1 > 2.2.0

--- version.py
+++ version.py
@@ -5,4 +5,4 @@ class Version:
     """A simple class to hold the version of the program.
     This is used to avoid unnecessary imports"""
     def __init__(self):
-        self.version = "2.1.1"
+        self.version = "2.2.0"
